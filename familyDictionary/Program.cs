73ae122b8a5adf61207ffa9638360e1d8ab72291
RichFisher <rich.fishernc@gmail.com>
2019-03-11 20:30:39 -0500

    bros before code

--- familyDictionary/Program.cs
+++ familyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace familyDictionary
 {
@@ -6,7 +7,17 @@ namespace familyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Forest" }, { "age", "37" } });
+
+            var output = "";
+
+            foreach(var (relation, data) in myFamily)
+            {
+                output += $"{data["name"]} is my {relation} and is {data["age"]} years old.";
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
         }
     }
 }
